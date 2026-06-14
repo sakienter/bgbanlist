1ec2bf62fb5319d75370a91ecb21cc2906aac324
sakienter <23mm0402@moon.aichi-u.ac.jp>
2026-06-14 18:07:35 +0900

    Add ban list data part 3

---
+++ data/rows-03.js
@@ -0,0 +1,316 @@
+window.BG_BAN_ROWS = window.BG_BAN_ROWS.concat(
+[
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Gladiator’s Spoils",
+    "restriction": "ヒーローBAN",
+    "target": "Onyxia",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Match Fixing",
+    "restriction": "ヒーローBAN",
+    "target": "Pyramad",
+    "status": "公式履歴",
+    "note": "32.0.3追加"
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Money Match",
+    "restriction": "ヒーローBAN",
+    "target": "Trade Prince Gallywix",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Galakrond",
+    "restriction": "ヒーローBAN",
+    "target": "Patches the Pirate",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Galakrond",
+    "restriction": "ヒーローBAN",
+    "target": "Lich Baz’hial",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Galakrond",
+    "restriction": "ヒーローBAN",
+    "target": "Infinite Toki",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Galakrond",
+    "restriction": "ヒーローBAN",
+    "target": "Millhouse Manastorm",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Galakrond",
+    "restriction": "ヒーローBAN",
+    "target": "Captain Eudora",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Galakrond",
+    "restriction": "ヒーローBAN",
+    "target": "Chenvaala",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Galakrond",
+    "restriction": "ヒーローBAN",
+    "target": "Heistbaron Togwaggle",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Galakrond",
+    "restriction": "ヒーローBAN",
+    "target": "Cap’n Hoggarr",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Packed Stands",
+    "restriction": "ヒーローBAN",
+    "target": "Trade Prince Gallywix",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Packed Stands",
+    "restriction": "ヒーローBAN",
+    "target": "Shudderwock",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Packed Stands",
+    "restriction": "ヒーローBAN",
+    "target": "Lich Baz’hial",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Packed Stands",
+    "restriction": "ヒーローBAN",
+    "target": "Infinite Toki",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Packed Stands",
+    "restriction": "ヒーローBAN",
+    "target": "Millhouse Manastorm",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Packed Stands",
+    "restriction": "ヒーローBAN",
+    "target": "Ysera",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Packed Stands",
+    "restriction": "ヒーローBAN",
+    "target": "Chenvaala",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Packed Stands",
+    "restriction": "ヒーローBAN",
+    "target": "Silas Darkmoon",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Packed Stands",
+    "restriction": "ヒーローBAN",
+    "target": "Guff Runetotem",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Packed Stands",
+    "restriction": "ヒーローBAN",
+    "target": "Overlord Saurfang",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Packed Stands",
+    "restriction": "ヒーローBAN",
+    "target": "Cookie the Cook",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Packed Stands",
+    "restriction": "ヒーローBAN",
+    "target": "Varden Dawngrasp",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Packed Stands",
+    "restriction": "ヒーローBAN",
+    "target": "Heistbaron Togwaggle",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Path of the Treasure-Seeker",
+    "restriction": "ヒーローBAN",
+    "target": "Lich Baz’hial",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Path of the Treasure-Seeker",
+    "restriction": "ヒーローBAN",
+    "target": "C’Thun",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Path of the Treasure-Seeker",
+    "restriction": "ヒーローBAN",
+    "target": "Tess Greymane",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Path of the Treasure-Seeker",
+    "restriction": "ヒーローBAN",
+    "target": "Ysera",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Path of the Treasure-Seeker",
+    "restriction": "ヒーローBAN",
+    "target": "Nozdormu",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Path of the Treasure-Seeker",
+    "restriction": "ヒーローBAN",
+    "target": "Scabbs Cutterbutter",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "E. T.C., Band Manager",
+    "anomaly": "Path of the Treasure-Seeker",
+    "restriction": "ヒーローBAN",
+    "target": "Varden Dawngrasp",
+    "status": "公式履歴",
+    "note": ""
+  },
+  {
+    "section": "A. F. Kay",
+    "anomaly": "Secrets of Norgannon",
+    "restriction": "ヒーローBAN",
+    "target": "Guff Runetotem",
+    "status": "現行確認",
+    "note": "32.0.3で公式追加"
+  },
+  {
+    "section": "A. F. Kay",
+    "anomaly": "Treasure Hoard",
+    "restriction": "ヒーローBAN",
+    "target": "Rock Master Voone",
+    "status": "現行確認",
+    "note": "31.6.2追加"
+  },
+  {
+    "section": "A. F. Kay",
+    "anomaly": "No Face, No Case",
+    "restriction": "ヒーローBAN",
+    "target": "Rock Master Voone",
+    "status": "現行確認",
+    "note": ""
+  },
+  {
+    "section": "A. F. Kay",
+    "anomaly": "Galakrond",
+    "restriction": "ヒーローBAN",
+    "target": "Dinotamer Brann",
+    "status": "現行確認",
+    "note": ""
+  },
+  {
+    "section": "A. F. Kay",
+    "anomaly": "Galakrond",
+    "restriction": "ヒーローBAN",
+    "target": "Rock Master Voone",
+    "status": "現行確認",
+    "note": ""
+  },
+  {
+    "section": "F. 偽りの偶像",
+    "anomaly": "偽りの偶像",
+    "restriction": "ヒーローBAN履歴",
+    "target": "Shudderwock",
+    "status": "公式履歴/現行一部確認",
+    "note": ""
+  },
+  {
+    "section": "F. 偽りの偶像",
+    "anomaly": "偽りの偶像",
+    "restriction": "ヒーローBAN履歴",
+    "target": "Maiev Shadowsong",
+    "status": "公式履歴/現行一部確認",
+    "note": ""
+  },
+  {
+    "section": "F. 偽りの偶像",
+    "anomaly": "偽りの偶像",
+    "restriction": "ヒーローBAN履歴",
+    "target": "Captain Eudora",
+    "status": "公式履歴/現行一部確認",
+    "note": ""
+  }
+]
+);
